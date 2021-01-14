d99f2dbbbc7c4915416b2bd3e6b94df1407a3207
Falcion <57592842+Falcion@users.noreply.github.com>
2021-01-14 10:41:17 +0300

    Updated modification version information

--- src/main/java/com/falcion/mortem/Lore.java
+++ src/main/java/com/falcion/mortem/Lore.java
@@ -4,5 +4,5 @@ public class Lore {
 
     public static final String ID = "mortem";
     public static final String NAME = "Mortem";
-    public static final String VERSION = "2.4.1";
+    public static final String VERSION = "2.4.1b";
 }
