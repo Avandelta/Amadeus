e133e97e189ee675a9342eab5e2267636860c305
Falcion <57592842+Falcion@users.noreply.github.com>
2021-01-30 15:25:55 +0300

    Full-rework of modification

--- src/main/java/net/deltamine/ru/Lore.java
+++
@@ -1,8 +0,0 @@
-package net.deltamine.ru;
-
-public class Lore {
-
-    public static final String ID = "mortem";
-    public static final String NAME = "Mortem";
-    public static final String VERSION = "2.4.1с";
-}
