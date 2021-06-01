2f17375afe75413ef7811f3bb4891d978fab83a0
Falcion <57592842+Falcion@users.noreply.github.com>
2021-06-01 18:47:20 +0300

    Clear the repository due refactoring of product
    - Final version will be released with development build snapshots.

--- src/main/java/deltamine/ru/Lore.java
+++
@@ -1,9 +0,0 @@
-package deltamine.ru;
-
-public class Lore {
-    public static final String ID = "pidlogger";
-    public static final String NAME = "PID Logger";
-    public static final String VERSION = "1.0";
-    public static final String CLIENT = "deltamine.ru.proxy.ClientProxy";
-    public static final String COMMON = "deltamine.ru.proxy.CommonProxy";
-}
